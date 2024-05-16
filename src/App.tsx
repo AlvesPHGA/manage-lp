6510d98dc77d08799aaab0a97b3fd38fd7a72ea3
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-05-16 11:17:22 -0300

    feat: add Home on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Home } from './ui/pages/Home';
 import { Footer } from './ui/partials/global/Footer';
 import { Header } from './ui/partials/global/Header';
 
@@ -5,6 +6,7 @@ function App() {
    return (
       <>
          <Header />
+         <Home />
          <Footer />
       </>
    );
