9e8bf85bfb1f9ee5e32edd23d4293e890abd9c1a
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-05-17 11:50:43 -0300

    feat: create ButtonComponent

---
+++ src/ui/components/form/ButtonComponent.tsx
@@ -0,0 +1,3 @@
+export const ButtonComponent = ({ ...props }) => {
+   return <button {...props} />;
+};
--- src/ui/partials/global/Header.tsx
+++ src/ui/partials/global/Header.tsx
@@ -1,3 +1,15 @@
+import { ButtonComponent } from '../../components/form/ButtonComponent';
+import { NavComponent } from '../../components/header/NavComponent';
+import { LogoSiteComponent } from '../../components/logo/LogoSiteComponent';
+
 export const Header = () => {
-   return <header>Header</header>;
+   return (
+      <header>
+         <div className="max-w-7xl mx-auto py-10 border flex justify-center">
+            <LogoSiteComponent />
+            <NavComponent />
+            <ButtonComponent>Get Started</ButtonComponent>
+         </div>
+      </header>
+   );
 };
