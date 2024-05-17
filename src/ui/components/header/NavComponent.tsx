b49ada6d3f2badce20963963e24a684332f7965e
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-05-17 11:36:22 -0300

    feat: create NavComponent

---
+++ src/ui/components/header/NavComponent.tsx
@@ -0,0 +1,11 @@
+export const NavComponent = () => {
+   return (
+      <nav>
+         <a href="#">Pricing</a>
+         <a href="#">Product</a>
+         <a href="#">About Us</a>
+         <a href="#">Careers</a>
+         <a href="#">Community</a>
+      </nav>
+   );
+};
