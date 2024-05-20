8c709fafd854edeb089b35254a8cfc065c564748
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-05-20 11:33:00 -0300

    style: nav on header

--- src/ui/components/header/NavComponent.tsx
+++ src/ui/components/header/NavComponent.tsx
@@ -1,11 +1,36 @@
 export const NavComponent = () => {
    return (
       <nav className="space-x-10">
-         <a href="#">Pricing</a>
-         <a href="#">Product</a>
-         <a href="#">About Us</a>
-         <a href="#">Careers</a>
-         <a href="#">Community</a>
+         <a
+            className="text-veryDarkBlue font-medium transition hover:opacity-80"
+            href="#"
+         >
+            Pricing
+         </a>
+         <a
+            className="text-veryDarkBlue font-medium transition hover:opacity-80"
+            href="#"
+         >
+            Product
+         </a>
+         <a
+            className="text-veryDarkBlue font-medium transition hover:opacity-80"
+            href="#"
+         >
+            About Us
+         </a>
+         <a
+            className="text-veryDarkBlue font-medium transition hover:opacity-80"
+            href="#"
+         >
+            Careers
+         </a>
+         <a
+            className="text-veryDarkBlue font-medium transition hover:opacity-80"
+            href="#"
+         >
+            Community
+         </a>
       </nav>
    );
 };
