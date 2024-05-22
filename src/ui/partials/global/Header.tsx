6b6c2767e88c1d3d0f56cc8c1058813b6f66775d
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-05-22 11:35:24 -0300

    style: create a style doc to header

--- src/ui/partials/global/Header.tsx
+++ src/ui/partials/global/Header.tsx
@@ -2,10 +2,14 @@ import { ButtonComponent } from '../../components/form/ButtonComponent';
 import { NavComponent } from '../../components/header/NavComponent';
 import { LogoSiteComponent } from '../../components/logo/LogoSiteComponent';
 
+import { headerStyle } from '../../styles/headerStyle';
+
+const { header } = headerStyle();
+
 export const Header = () => {
    return (
       <header>
-         <div className="max-w-7xl mx-auto py-12 flex items-center justify-between">
+         <div className={header()}>
             <LogoSiteComponent fill="#242D52" />
             <NavComponent />
             <ButtonComponent>Get Started</ButtonComponent>
---
+++ src/ui/styles/headerStyle.tsx
@@ -0,0 +1,7 @@
+import { tv } from 'tailwind-variants';
+
+export const headerStyle = tv({
+   slots: {
+      header: 'max-w-7xl mx-auto py-12 flex items-center justify-between',
+   },
+});
