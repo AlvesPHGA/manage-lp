7fd343c8de64a1d107436b270eb212a07ccdbb0b
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-05-24 11:06:27 -0300

    storybook: create a stories to header

---
+++ src/ui/stories/header/header.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Header } from '../../partials/global/Header';
+
+export default {
+   title: 'Partials/Header',
+   component: Header,
+} as Meta;
+
+export const Default: StoryObj = {};
