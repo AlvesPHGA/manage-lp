0d88a776b7751735e3dc046f0719b6b2ce679454
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-05-24 11:42:51 -0300

    storybook: create a stories to NavComponent

---
+++ src/ui/stories/nav/nav.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { NavComponent } from '../../components/header/NavComponent';
+
+export default {
+   title: 'Components/Nav',
+   component: NavComponent,
+} as Meta;
+
+export const Default: StoryObj = {};
