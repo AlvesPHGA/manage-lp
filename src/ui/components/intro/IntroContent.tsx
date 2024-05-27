7c4163d72d8459e00387f82af1f971a40d3dbae5
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-05-27 10:40:34 -0300

    style: IntroContent

--- src/ui/components/intro/IntroContent.tsx
+++ src/ui/components/intro/IntroContent.tsx
@@ -2,14 +2,18 @@ import { ButtonComponent } from '../form/ButtonComponent';
 
 export const IntroContent = () => {
    return (
-      <div className="w-[40%]">
-         <h1 className="text-5xl font-bold">
-            Bring everyone together to build better products.
-         </h1>
-         <p>
-            Manage makes it simple for software teams to plan day-to-day tasks
-            while keeping the larger team goals in view.
-         </p>
+      <div className="space-y-10">
+         <div className="w-[40%]">
+            <h1 className="text-6xl font-bold text-darkBlue tracking-tight leading-[68px]">
+               Bring everyone together to build better products.
+            </h1>
+         </div>
+         <div className="w-[31%]">
+            <p className="text-lg text-darkGrayishBlue font-normal">
+               Manage makes it simple for software teams to plan day-to-day
+               tasks while keeping the larger team goals in view.
+            </p>
+         </div>
          <ButtonComponent />
       </div>
    );
