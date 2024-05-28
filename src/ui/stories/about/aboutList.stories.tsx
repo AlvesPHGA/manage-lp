2618045cf3cae45dce1752cb50f755b1504d3040
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-05-28 16:42:36 -0300

    stories: create a stories to AboutList

---
+++ src/ui/stories/about/aboutList.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { AboutList } from '../../components/about/AboutList';
+
+export default {
+   title: 'Components/AboutList',
+   component: AboutList,
+} as Meta;
+
+export const List: StoryObj = {};
