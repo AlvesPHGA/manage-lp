194bef7264035ad2c5730a8be816319ace2d0eb8
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-05-28 11:40:47 -0300

    storybook: create a stories to Intro section

---
+++ src/ui/stories/intro/intro.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Intro } from '../../partials/home/Intro';
+
+export default {
+   title: 'Partials/Intro',
+   component: Intro,
+} as Meta;
+
+export const Default: StoryObj = {};
