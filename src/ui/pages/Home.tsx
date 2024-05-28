cc5f5dcf2aa2508167ce67b638ab9db577af4ac8
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-05-28 17:00:14 -0300

    feat: Home

--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -1,15 +1,11 @@
 import { About } from '../partials/home/About';
-import { Discussions } from '../partials/home/Discussions';
 import { Intro } from '../partials/home/Intro';
-import { Service } from '../partials/home/Service';
 
 export const Home = () => {
    return (
       <>
          <Intro />
          <About />
-         <Discussions />
-         <Service />
       </>
    );
 };
