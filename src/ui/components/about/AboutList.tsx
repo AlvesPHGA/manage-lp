c433388be306a19fa3c25ce9d86fb92c1a6a1631
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-05-28 12:18:38 -0300

    feat: create AboutList componet

---
+++ src/ui/components/about/AboutList.tsx
@@ -0,0 +1,38 @@
+export const AboutList = () => {
+   return (
+      <ul>
+         <dl>
+            <dt>
+               <span>01</span>
+               Track company-wide progress
+            </dt>
+            <dd>
+               See how your day-to-day tasks fit into the wider vision. Go from
+               tracking progress at the milestone level all the way done to the
+               smallest of details. Never lose sight of the bigger picture
+               again.
+            </dd>
+
+            <dt>
+               <span>02</span>
+               Advanced built-in reports
+            </dt>
+            <dd>
+               Set internal delivery estimates and track progress toward company
+               goals. Our customisable dashboard helps you build out the reports
+               you need to keep key stakeholders informed.
+            </dd>
+
+            <dt>
+               {' '}
+               <span>03</span> Everything you need in one place
+            </dt>
+            <dd>
+               Stop jumping from one service to another to communicate, store
+               files, track tasks and share documents. Manage offers an
+               all-in-one team productivity solution.
+            </dd>
+         </dl>
+      </ul>
+   );
+};
--- src/ui/partials/home/About.tsx
+++ src/ui/partials/home/About.tsx
@@ -1,10 +1,12 @@
 import { AboutContent } from '../../components/about/AboutContent';
+import { AboutList } from '../../components/about/AboutList';
 
 export const About = () => {
    return (
       <section>
          <div>
             <AboutContent />
+            <AboutList />
          </div>
       </section>
    );
