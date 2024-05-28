eadb7a6a8f2fce251f0582e7af4401fd20e7e833
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-05-27 22:29:00 -0300

    feat: intro partial

--- src/ui/components/intro/IntroContent.tsx
+++ src/ui/components/intro/IntroContent.tsx
@@ -2,13 +2,13 @@ import { ButtonComponent } from '../form/ButtonComponent';
 
 export const IntroContent = () => {
    return (
-      <div className="space-y-10">
-         <div className="w-[40%]">
+      <div className="space-y-10 w-[50%]">
+         <div className="w-[90%]">
             <h1 className="text-6xl font-bold text-darkBlue tracking-tight leading-[68px]">
                Bring everyone together to build better products.
             </h1>
          </div>
-         <div className="w-[31%]">
+         <div className="w-[60%]">
             <p className="text-lg text-darkGrayishBlue font-normal">
                Manage makes it simple for software teams to plan day-to-day
                tasks while keeping the larger team goals in view.
--- src/ui/partials/home/Intro.tsx
+++ src/ui/partials/home/Intro.tsx
@@ -3,8 +3,14 @@ import { IntroContent } from '../../components/intro/IntroContent';
 export const Intro = () => {
    return (
       <section className="py-20">
-         <div className="max-w-7xl mx-auto">
+         <div className="max-w-7xl mx-auto flex justify-between items-center">
             <IntroContent />
+
+            <img
+               className="w-[620px]"
+               src="./icons/illustration-intro.svg"
+               alt="image some datas in percents about services of site"
+            />
          </div>
       </section>
    );
