53446eff99537ebb853d3b0be488d8839dc90200
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-05-28 16:39:05 -0300

    stories: create a stories to About section

---
+++ src/ui/stories/about/about.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { About } from '../../partials/home/About';
+
+export default {
+   title: 'Partials/About',
+   component: About,
+} as Meta;
+
+export const AboutComponent: StoryObj = {};
