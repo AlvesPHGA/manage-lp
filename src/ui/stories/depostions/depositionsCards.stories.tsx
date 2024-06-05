332478900a6072ac5f06e8ee411d6997ebb4025a
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-06-05 11:43:36 -0300

    storybook: create a new stories to DepositionsCards component

---
+++ src/ui/stories/depostions/depositionsCards.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { DepositionsCards } from '../../components/depositions/DepositionsCards';
+
+export default {
+   title: 'Components/DepositionsCards',
+   component: DepositionsCards,
+} as Meta;
+
+export const DepositionCards: StoryObj = {};
