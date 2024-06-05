749636bfc5aa62b439e71afb8f06add97c22483a
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-06-05 11:39:00 -0300

    storybook: create a storie to Depositions section

---
+++ src/ui/stories/depostions/depositions.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Depositions } from '../../partials/home/Depositions';
+
+export default {
+   title: 'Partials/Depositions',
+   componet: Depositions,
+} as Meta;
+
+export const Deposition: StoryObj = {};
