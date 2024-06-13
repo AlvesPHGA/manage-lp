687c46d128c3edd8e1409b46053c3ff3fcacc6c2
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-06-13 12:18:09 -0300

    storybook: create a stories to works section

---
+++ src/ui/stories/works/works.stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Works } from '../../partials/home/Works';
+
+export default {
+   title: 'Partials/Works',
+   component: Works,
+} as Meta;
+
+export const WorksPartials: StoryObj = {};
