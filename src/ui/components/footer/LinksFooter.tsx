c330d8afd95c54b3f793ef510931ca3043dcdc70
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-06-14 15:38:12 -0300

    feat: linksFooter

--- src/ui/components/footer/LinksFooter.tsx
+++ src/ui/components/footer/LinksFooter.tsx
@@ -1,28 +1,49 @@
 export const LinksFooter = () => {
    return (
-      <div className="flex space-x-24">
+      <div className="flex space-x-40">
          <div className="flex flex-col gap-4">
-            <a href="" className="text-white text-base">
+            <a
+               href=""
+               className="text-white text-base hover:text-brightRed transition"
+            >
                Home
             </a>
-            <a href="" className="text-white text-base">
+            <a
+               href=""
+               className="text-white text-base hover:text-brightRed transition"
+            >
                Pricing
             </a>
-            <a href="" className="text-white text-base">
+            <a
+               href=""
+               className="text-white text-base hover:text-brightRed transition"
+            >
                Products
             </a>
-            <a href="" className="text-white text-base">
+            <a
+               href=""
+               className="text-white text-base hover:text-brightRed transition"
+            >
                About Us
             </a>
          </div>
          <div className="flex flex-col gap-4">
-            <a href="" className="text-white text-base">
+            <a
+               href=""
+               className="text-white text-base hover:text-brightRed transition"
+            >
                Careers
             </a>
-            <a href="" className="text-white text-base">
+            <a
+               href=""
+               className="text-white text-base hover:text-brightRed transition"
+            >
                Community
             </a>
-            <a href="" className="text-white text-base">
+            <a
+               href=""
+               className="text-white text-base hover:text-brightRed transition"
+            >
                Privacy Policy
             </a>
          </div>
