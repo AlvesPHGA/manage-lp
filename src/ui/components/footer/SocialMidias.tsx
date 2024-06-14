488f30ebe71ed6c52def10b0d14d925f56bf14dd
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-06-14 12:15:30 -0300

    feat: create two component SocialMedias and IconsSocialMedia

---
+++ src/ui/components/footer/SocialMidias.tsx
@@ -0,0 +1,19 @@
+import {
+   Facebook,
+   Instagram,
+   Pinterest,
+   Twitter,
+   Youtube,
+} from '../logo/IconsSocialMedia';
+
+export const SocialMidias = () => {
+   return (
+      <>
+         <Facebook />
+         <Youtube />
+         <Twitter />
+         <Pinterest />
+         <Instagram />
+      </>
+   );
+};
