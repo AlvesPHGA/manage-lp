bea29ebb60941f9100ee3a77f0a95e56509e1820
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-06-14 15:35:25 -0300

    feat: footer

--- src/ui/components/footer/FormComponent.tsx
+++ src/ui/components/footer/FormComponent.tsx
@@ -2,9 +2,11 @@ import { InputComponent } from '../form/InputComponent';
 
 export const FormComponent = () => {
    return (
-      <form action="">
+      <form className="space-x-4">
          <InputComponent />
-         <button>Go</button>
+         <button className="border border-brightRed bg-brightRed py-3.5 px-6 rounded-[26px] text-white font-medium hover:text-brightRed hover:bg-transparent transition">
+            Go
+         </button>
       </form>
    );
 };
--- src/ui/partials/global/Footer.tsx
+++ src/ui/partials/global/Footer.tsx
@@ -5,14 +5,19 @@ import { LogoSiteComponent } from '../../components/logo/LogoSiteComponent';
 
 export const Footer = () => {
    return (
-      <footer className="bg-darkBlue py-20">
+      <footer className="bg-veryDarkBlue py-20">
          <div className="max-w-7xl mx-auto flex justify-between items-center">
             <div className="space-y-20">
                <LogoSiteComponent fill="#fff" />
                <SocialMediasComponent />
             </div>
             <LinksFooter />
-            <FormComponent />
+            <div className="text-right flex flex-col gap-16">
+               <FormComponent />
+               <span className="text-xs text-darkGrayishBlue">
+                  Copyright 2020. All Rights Reserved
+               </span>
+            </div>
          </div>
       </footer>
    );
