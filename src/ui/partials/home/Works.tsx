c21e34f1b481671e7b09c9f0ccbbfa1ce86f83ff
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-06-14 16:25:13 -0300

    feat: works section

--- src/ui/partials/home/Works.tsx
+++ src/ui/partials/home/Works.tsx
@@ -2,7 +2,7 @@ import { WorksContent } from '../../components/works/worksContent';
 
 export const Works = () => {
    return (
-      <section className="relative border py-20 bg-brightRed bg-[url('./icons/bg-simplify-section-desktop.svg')] bg-no-repeat bg-rb-screen-1200plus bg-bs-1200plus">
+      <section className="relative py-20 bg-brightRed bg-[url('./icons/bg-simplify-section-desktop.svg')] bg-no-repeat bg-rb-screen-1200plus bg-bs-1200plus">
          <WorksContent />
       </section>
    );
