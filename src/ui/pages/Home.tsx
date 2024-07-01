b689bb9203c3dfaf0c285a446d72c93df751a83d
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-07-01 11:47:13 -0300

    style: update background image of home page

--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -7,7 +7,7 @@ export const Home = () => {
    return (
       <>
          <Intro />
-         <div className="h-fit bg-backSiteImg bg-no-repeat bg-bttmBp-pattern-1200plus bg-bttmBs-pattern-1200plus">
+         <div className="bg-backSiteImg bg-no-repeat h-fit bg-bttmBp-pattern-1200plus bg-bttmBs-pattern-1200plus">
             <About />
             <Depositions />
          </div>
