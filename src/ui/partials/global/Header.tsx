0c1d9391e5c054c38fd28dc4d5a720cf5c9e9253
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-07-04 19:05:16 -0300

    feat: add a menu of lucide

--- src/ui/partials/global/Header.tsx
+++ src/ui/partials/global/Header.tsx
@@ -1,10 +1,10 @@
-import { ReactSVG } from 'react-svg';
 import { CTAComponent } from '../../components/cta/CTAComponent';
 import { NavComponent } from '../../components/header/NavComponent';
 import { LogoSiteComponent } from '../../components/logo/LogoSiteComponent';
 import { ctaStyle } from '../../styles/ctaStyle';
 
 import { headerStyle } from '../../styles/headerStyle';
+import { Menu } from 'lucide-react';
 
 const { header } = headerStyle();
 
@@ -13,10 +13,8 @@ export const Header = () => {
       <header>
          <div className={header()}>
             <LogoSiteComponent fill="#242D52" />
-            <ReactSVG
-               className="hidden dmd:block"
-               src="./icons/icon-hamburger.svg"
-            />
+            <Menu className="hidden dmd:block dmd:size-12 dmd:mb-5" />
+
             <div className="flex items-center gap-64 dlg:gap-20 dmd:hidden">
                <NavComponent />
                <CTAComponent className={ctaStyle({ cta: 'primary' })} />
